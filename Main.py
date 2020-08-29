3d94ec3ae0f4ae781cbec9458ac1a2af141133e4
Nitro5176 <st.98.30.03@gmail.com>
2020-08-29 15:16:41 -0400

    Added: Screen size Title screen will be locked at 30fps exit is added in the event loop background is black

--- Main.py
+++ Main.py
@@ -1,6 +1,12 @@
 import pygame
 
 pygame.init()
+#size of the window
+window = pygame.display.set_mode((600, 600))
+#display name
+pygame.display.set_caption("Covid Defender")
+#clock (for frames per second)
+clock = pygame.time.Clock()
 
 print("Welcome to CovidDefender!")
 
@@ -8,8 +14,20 @@ print("Welcome to CovidDefender!")
 running = True
 
 def main():
+    #makes the variable running global.
+    global running
 
     while running:
+        #frames:
+        clock.tick(30)
 
+        #to exit the game:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        #makes the wallpaper black
+        window.fill((0,0,0))
 
-
+#calling the main method
+main()
+pygame.quit()
